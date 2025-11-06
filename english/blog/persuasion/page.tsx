b208527ea0a6177ebfb84dcdfcee27c535a1e6d9
KonstantinDuda <52273304+KonstantinDuda@users.noreply.github.com>
2025-11-06 09:23:31 +0200

    chapter_4

--- english/blog/persuasion/page.tsx
+++ english/blog/persuasion/page.tsx
@@ -7,6 +7,8 @@ import PersuasionFour from '../../posts/persuasion/four';
 import PersuasionFive from '../../posts/persuasion/five';
 import PersuasionSix from '../../posts/persuasion/six';
 import PersuasionSeven from '../../posts/persuasion/seven';
+import PersuasionEight from '../../posts/persuasion/eight';
+//import PersuasionNine from '../../posts/persuasion/nine';
 
 export default function Page() {
     return (
@@ -15,6 +17,8 @@ export default function Page() {
             <h3>by Jane Austin. Release date: February 1, 1994</h3>
             <h3>Читаю завдяки <a href="https://www.gutenberg.org/ebooks/105"
                 target="_blank" rel='noreferrer'>Project Gutenberg</a>.</h3>
+            {/*<PersuasionNine />*/}
+            <PersuasionEight />
             <PersuasionSeven />
             <PersuasionSix />
             <PersuasionFive />
--- english/posts/persuasion/five.tsx
+++ english/posts/persuasion/five.tsx
@@ -6,134 +6,132 @@ import style from "@/app/ui/english/blog/blog.module.css";
 export default function PersuasionFive() {
     return (
         <>
-            <ul>
-                <li>
-                    <a href='https://dictionary.cambridge.org/dictionary/english-ukrainian/cautious'
-                        title="Обережний. Передбачливий"
-                        target='_blank' rel='noreferrer'>cautious</a> - Having or showing
-                    caution. Careful
-                </li>
-                <li>
-                    <a href='https://dictionary.cambridge.org/dictionary/english-ukrainian/anxiously'
-                        title="Стурбовано, схвильовано"
-                        target='_blank' rel='noreferrer'>anxiously</a> - <i>They&#39;re
-                            were waiting anxiously for news about their son
-                    </i>
-                </li>
-                <li>
-                    <a href='https://dictionary.cambridge.org/dictionary/english-ukrainian/zealous'
-                        title="Старанний, завзятий"
-                        target='_blank' rel='noreferrer'>zealous</a> - Enthusiastic. Keen
-                </li>
-                <li>
-                    <a href='https://dictionary.cambridge.org/dictionary/english-ukrainian/strict'
-                        title="Суворий, вимогливий. Точний, певний"
-                        target='_blank' rel='noreferrer'>strict</a> - Severe, stern, and
-                    compelling obedience. Exact or precise
-                </li>
-                <li>
-                    <a href='https://dictionary.cambridge.org/dictionary/english-ukrainian/integrity'
-                        title="Цілісність. Незайманість"
-                        target='_blank' rel='noreferrer'>integrity</a> - Honestly
-                </li>
-                <li>
-                    <a href='https://dictionary.cambridge.org/dictionary/english-ukrainian/benevolent'
-                        title="Доброзичливий, милосердний"
-                        target='_blank' rel='noreferrer'>benevolent</a> - <i>The company
-                            donated a large sum of money to local charities as a benevolent
-                            gesture
-                    </i>
-                </li>
-                <li>
-                    <a href='https://dictionary.cambridge.org/dictionary/english-ukrainian/prejudices'
-                        title="Упередження, забобони. Викликати упередження. Завдавати шкоди"
-                        target='_blank' rel='noreferrer'>prejudices</a> - (an) Opinion or
-                    feeling for or especially against something, formed unfairly or
-                    unresonable ie without proper knowledge. To cause to feel
-                    prejudice for or against something. To harm or endanger (a
-                    person&#39;s position, prospects etc) in some way
-                </li>
-                <li>
-                    <a href='https://dictionary.cambridge.org/dictionary/english-ukrainian/doubt'
-                        title="Сумніватися, вагатися. Бути не впевненим, не довіряти"
-                        target='_blank' rel='noreferrer'>doubt</a> - To feel uncertain
-                    about, but inclined not to believe. Not to be sure of the
-                    reliability of. F feeling of not being sure and sometimes of
-                    being sespecious
-                </li>
-                <li>
-                    <a href='https://dictionary.cambridge.org/dictionary/english-ukrainian/due'
-                        title="Зобов'язаний. Очікуваний, такий що має бути. Належний, гідний. Податки, митні збори. Прямо, точно."
-                        target='_blank' rel='noreferrer'>due</a> - Owed. Expected
-                    according to timetable, promise etc. Proper. Directly South. What is
-                    owed, especially what one has a right to. Charge, fee or toll.
-                </li>
-                <li>
-                    <a href='https://dictionary.cambridge.org/dictionary/english-ukrainian/urging'
-                        title="Підганяти, спонукати. Переконувати, наполягати. Сильне бажання"
-                        target='_blank' rel='noreferrer'>urging (urge)</a> - To try to
-                    persuade or request earnestly (someone to do something). To try to
-                    convince a person of (eg the importance of, necessity for, some
-                    acting). A strong impulse or desire
-                </li>
-                <li>
-                    <a href='https://dictionary.cambridge.org/dictionary/english/retrenchment'
-                        title="Скорочення"
-                        target='_blank' rel='noreferrer'>retrenchments</a> - A situation in
-                    which a government, etc. spends less or reduces costs. A situation in
-                    which someone loses their job because their employer does not need them
-                </li>
-                <li>
-                    <a href='https://dictionary.cambridge.org/dictionary/english-ukrainian/decenсy'
-                        title="Пристойність, порядність"
-                        target='_blank' rel='noreferrer'>decenсies</a> - (The gentral idea of)
-                    what is proper, fitting, moral etc. The quality or act of being decent
-                </li>
-                <li>
-                    <a href='https://dictionary.cambridge.org/dictionary/english-ukrainian/abode'
-                        title="Місцеперебування, житло"
-                        target='_blank' rel='noreferrer'>abode</a> - The place where you
-                    live.
-                </li>
-                <li>
-                    <a href='https://dictionary.cambridge.org/dictionary/english-ukrainian/undesirable'
-                        title="Небажаний, непридатний"
-                        target='_blank' rel='noreferrer'>undesirableness</a> - Not wantad.
-                    Unpleasant or objectionable
-                </li>
-                <li>
-                    <a href='https://dictionary.cambridge.org/dictionary/english-ukrainian/grafted'
-                        title="Пересаджувати, імплантувати. Хабар, хабарнитство. Наполеглива праця"
-                        target='_blank' rel='noreferrer'>engrafted</a> - Tofix (skin, bone etc)
-                    from one part of body on to or into another part of body. A piece of
-                    skin, bone etc which is grafted. Dishonesty in obtaining profit or
-                    good position. Hard work
-                </li>
-                <li title="Випробовування на мужність">a trial of <a
-                    href='https://dictionary.cambridge.org/dictionary/english-ukrainian/fortitude'
-                    title="Сила духу"
-                    target='_blank' rel='noreferrer'>fortitude</a> - Courage and endurance
-                </li>
-                <li>
-                    <a href='https://dictionary.cambridge.org/dictionary/english-ukrainian/observances'
-                        title="Додержання. Повага, пошана"
-                        target='_blank' rel='noreferrer'>observances</a> - The act of obeying
-                    rules etc. The act of observing (a tradition etc)
-                </li>
-                <li>
-                    <a href='https://dictionary.cambridge.org/dictionary/english-ukrainian/complaisance'
-                        title="Люб'язність, ввічливість, прислужливість, поступливість"
-                        target='_blank' rel='noreferrer'>complaisance</a> - Willingness to
-                    please others by being polite and fitting in with their plans
-                </li>
+            <div className={style.persuasionTwoFlex}>
+                <ul className={style.persuasionTwoText}>
+                    <li>
+                        <a href='https://dictionary.cambridge.org/dictionary/english-ukrainian/cautious'
+                            title="Обережний. Передбачливий"
+                            target='_blank' rel='noreferrer'>cautious</a> - Having or showing
+                        caution. Careful
+                    </li>
+                    <li>
+                        <a href='https://dictionary.cambridge.org/dictionary/english-ukrainian/anxiously'
+                            title="Стурбовано, схвильовано"
+                            target='_blank' rel='noreferrer'>anxiously</a> - <i>They&#39;re
+                                were waiting anxiously for news about their son
+                        </i>
+                    </li>
+                    <li>
+                        <a href='https://dictionary.cambridge.org/dictionary/english-ukrainian/zealous'
+                            title="Старанний, завзятий"
+                            target='_blank' rel='noreferrer'>zealous</a> - Enthusiastic. Keen
+                    </li>
+                    <li>
+                        <a href='https://dictionary.cambridge.org/dictionary/english-ukrainian/strict'
+                            title="Суворий, вимогливий. Точний, певний"
+                            target='_blank' rel='noreferrer'>strict</a> - Severe, stern, and
+                        compelling obedience. Exact or precise
+                    </li>
+                    <li>
+                        <a href='https://dictionary.cambridge.org/dictionary/english-ukrainian/integrity'
+                            title="Цілісність. Незайманість"
+                            target='_blank' rel='noreferrer'>integrity</a> - Honestly
+                    </li>
+                    <li>
+                        <a href='https://dictionary.cambridge.org/dictionary/english-ukrainian/benevolent'
+                            title="Доброзичливий, милосердний"
+                            target='_blank' rel='noreferrer'>benevolent</a> - <i>The company
+                                donated a large sum of money to local charities as a benevolent
+                                gesture
+                        </i>
+                    </li>
+                    <li>
+                        <a href='https://dictionary.cambridge.org/dictionary/english-ukrainian/prejudices'
+                            title="Упередження, забобони. Викликати упередження. Завдавати шкоди"
+                            target='_blank' rel='noreferrer'>prejudices</a> - (an) Opinion or
+                        feeling for or especially against something, formed unfairly or
+                        unresonable ie without proper knowledge. To cause to feel
+                        prejudice for or against something. To harm or endanger (a
+                        person&#39;s position, prospects etc) in some way
+                    </li>
+                    <li>
+                        <a href='https://dictionary.cambridge.org/dictionary/english-ukrainian/doubt'
+                            title="Сумніватися, вагатися. Бути не впевненим, не довіряти"
+                            target='_blank' rel='noreferrer'>doubt</a> - To feel uncertain
+                        about, but inclined not to believe. Not to be sure of the
+                        reliability of. F feeling of not being sure and sometimes of
+                        being sespecious
+                    </li>
+                    <li>
+                        <a href='https://dictionary.cambridge.org/dictionary/english-ukrainian/due'
+                            title="Зобов'язаний. Очікуваний, такий що має бути. Належний, гідний. Податки, митні збори. Прямо, точно."
+                            target='_blank' rel='noreferrer'>due</a> - Owed. Expected
+                        according to timetable, promise etc. Proper. Directly South. What is
+                        owed, especially what one has a right to. Charge, fee or toll.
+                    </li>
+                </ul>
+                <ul className={style.persuasionTwoImage}>
+                    <li>
+                        <a href='https://dictionary.cambridge.org/dictionary/english-ukrainian/urging'
+                            title="Підганяти, спонукати. Переконувати, наполягати. Сильне бажання"
+                            target='_blank' rel='noreferrer'>urging (urge)</a> - To try to
+                        persuade or request earnestly (someone to do something). To try to
+                        convince a person of (eg the importance of, necessity for, some
+                        acting). A strong impulse or desire
+                    </li>
+                    <li>
+                        <a href='https://dictionary.cambridge.org/dictionary/english/retrenchment'
+                            title="Скорочення"
+                            target='_blank' rel='noreferrer'>retrenchments</a> - A situation in
+                        which a government, etc. spends less or reduces costs. A situation in
+                        which someone loses their job because their employer does not need them
+                    </li>
+                    <li>
+                        <a href='https://dictionary.cambridge.org/dictionary/english-ukrainian/decenсy'
+                            title="Пристойність, порядність"
+                            target='_blank' rel='noreferrer'>decenсies</a> - (The gentral idea of)
+                        what is proper, fitting, moral etc. The quality or act of being decent
+                    </li>
+                    <li>
+                        <a href='https://dictionary.cambridge.org/dictionary/english-ukrainian/abode'
+                            title="Місцеперебування, житло"
+                            target='_blank' rel='noreferrer'>abode</a> - The place where you
+                        live.
+                    </li>
+                    <li>
+                        <a href='https://dictionary.cambridge.org/dictionary/english-ukrainian/undesirable'
+                            title="Небажаний, непридатний"
+                            target='_blank' rel='noreferrer'>undesirableness</a> - Not wantad.
+                        Unpleasant or objectionable
+                    </li>
+                    <li>
+                        <a href='https://dictionary.cambridge.org/dictionary/english-ukrainian/grafted'
+                            title="Пересаджувати, імплантувати. Хабар, хабарнитство. Наполеглива праця"
+                            target='_blank' rel='noreferrer'>engrafted</a> - Tofix (skin, bone etc)
+                        from one part of body on to or into another part of body. A piece of
+                        skin, bone etc which is grafted. Dishonesty in obtaining profit or
+                        good position. Hard work
+                    </li>
+                    <li title="Випробовування на мужність">a trial of <a
+                        href='https://dictionary.cambridge.org/dictionary/english-ukrainian/fortitude'
+                        title="Сила духу"
+                        target='_blank' rel='noreferrer'>fortitude</a> - Courage and endurance
+                    </li>
+                    <li>
+                        <a href='https://dictionary.cambridge.org/dictionary/english-ukrainian/observances'
+                            title="Додержання. Повага, пошана"
+                            target='_blank' rel='noreferrer'>observances</a> - The act of obeying
+                        rules etc. The act of observing (a tradition etc)
+                    </li>
+                    <li>
+                        <a href='https://dictionary.cambridge.org/dictionary/english-ukrainian/complaisance'
+                            title="Люб'язність, ввічливість, прислужливість, поступливість"
+                            target='_blank' rel='noreferrer'>complaisance</a> - Willingness to
+                        please others by being polite and fitting in with their plans
+                    </li>
 
-
-                {/*<li>
-                <a href='https://dictionary.cambridge.org/dictionary/english-ukrainian/'
-                title=""
-                target='_blank' rel='noreferrer'></a>
-                        </li>*/}
-            </ul >
+                </ul >
+            </ div>
             <h3>Capter 2 &#8657;</h3>
             <div className={style.idDate}>22.10.2025 - 28.10.2025</div>
         </>
